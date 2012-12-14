5f6c6c9fdb29a7725c6af6ddce7e76e0a325b59c
tyna <liadoon@gmail.com>
2012-12-14 12:43:51 +0100

    integers on generator output, separate limits for knapsack and items size

--- src/main/resources/scripts/generator.py
+++ src/main/resources/scripts/generator.py
@@ -1,44 +1,30 @@
+#!/usr/bin/env python
+
 import random
 from sys import argv, stdout
 from functools import reduce
 
 def usage():
-    print("Usage:")
-    print("python generator.py dimensions items maximum")
+    print("Usage: python " + argv[0] + " dimensions items_num knapsack_max items_max")
     exit()
     
-def fprint(s):
-    print(s)
+def print_list(l):
+    print reduce(lambda x,y: str(x)+" "+str(y), l)
     
 try:
     dimensions = int(argv[1])
-    items = int(argv[2])
-    maximum = int(argv[3])
+    items_num = int(argv[2])
+    knapsack_max = int(argv[3])
+    items_max = int(argv[4])
 except Exception:
     usage()
+
+
+knapsack = [knapsack_max for x in range(dimensions)]
+items = [[random.randrange(1,items_max) for x in range(dimensions+1)]
+		for y in range(items_num)]
     
-print(str(dimensions)+" "+str(items))
-
-print(
-    reduce(
-        lambda x, y: str(x)+" "+str(y),
-        map(
-            lambda x: random.random() * maximum,
-            range(dimensions)
-        )
-    )
-)
-
-
-list(map(
-    lambda v: fprint(
-                reduce(
-                    lambda x, y: str(x)+" "+str(y),
-                    map(
-                        lambda z: random.random() * maximum,
-                        range(dimensions + 1)
-                    )
-                )
-            ),
-    range(items)
-))
+print(str(dimensions)+" "+str(items_num))
+print_list(knapsack)
+for item in items:
+	print_list(item)
